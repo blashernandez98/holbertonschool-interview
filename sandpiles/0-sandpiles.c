cf29844e20f746eb8d760300801395d315517b88
blas <blashernandez98@gmail.com>
2023-04-23 23:47:10 -0300

    Task0-Betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -8,42 +8,47 @@
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
-void toppling_round(int grid1[3][3])
+/**
+ * toppling_round - Performs one round of toppling
+ *
+ * @grid: Input sandpile
+ */
+void toppling_round(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-            {
-                grid1[i][j] -= 4;
-                if (i > 0)
-                    grid1[i - 1][j] += 1;
-                if (j > 0)
-                    grid1[i][j - 1] += 1;
-                if (i < 2)
-                    grid1[i + 1][j] += 1;
-                if (j < 2)
-                    grid1[i][j + 1] += 1;
-            }
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				grid[i][j] -= 4;
+				if (i > 0)
+					grid[i - 1][j] += 1;
+				if (j > 0)
+					grid[i][j - 1] += 1;
+				if (i < 2)
+					grid[i + 1][j] += 1;
+				if (j < 2)
+					grid[i][j + 1] += 1;
+			}
+		}
+	}
 }
 
 /**
@@ -55,33 +60,33 @@ void toppling_round(int grid1[3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    char stable = 0;
+	int i, j;
+	char stable = 0;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            grid1[i][j] += grid2[i][j];
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	}
 
-    while (!stable)
-    {
-        stable = 1;
-        for (i = 0; i < 3; i++)
-        {
-            if (!stable)
-                break;
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    printf("=\n");
-                    print_grid(grid1);
-                    toppling_round(grid1);
-                    stable = 0;
-                    break;
-                }
-            }
-        }
-    }
+	while (!stable)
+	{
+		stable = 1;
+		for (i = 0; i < 3; i++)
+		{
+			if (!stable)
+				break;
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					printf("=\n");
+					print_grid(grid1);
+					toppling_round(grid1);
+					stable = 0;
+					break;
+				}
+			}
+		}
+	}
 }
